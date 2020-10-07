676f98ee9f9d34236d0e0d986f9eb27566011d06
Jackson Bailey <jackson.bailey@ondotsystems.com>
2020-10-07 17:54:04 -0400

    Manage version of Spring Boot plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,9 @@ rootProject.name = "sheeper"
 include("sheeper-api")
 include("sheeper-internal-platform")
 include("sheeper-server")
+
+pluginManagement {
+  plugins {
+    id("org.springframework.boot") version "2.3.4.RELEASE"
+  }
+}
